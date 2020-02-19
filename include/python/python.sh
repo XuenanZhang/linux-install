aac8b893baa59771359150c59a691387c784839c
带头大哥 <176108053@qq.com>
2020-02-19 15:45:21 +0800

    Update python.sh

--- include/python/python.sh
+++ include/python/python.sh
@@ -2,11 +2,14 @@
 
 # install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel python-devel
 log_info "安装python基础"
-install python-devel python-pip zlib zlib-devel libffi-devel
+install python-devel zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel libffi-devel
 
 #安装python3
 log_info "安装python3.7"
-download https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
+cd ${cache_dir}
+if [ ! -s "Python-3.7.0.tar.xz" ];then
+    download https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
+fi
 tar -xvJf  Python-3.7.0.tar.xz
 cd Python-3.7.0
 ./configure --prefix=/usr/local/python3
@@ -15,9 +18,11 @@ sudo make install
 sudo ln -s /usr/local/python3/bin/python3 /usr/bin/python3
 sudo ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
 
-log_info "安装pip"
+log_info "更新pip3"
 sudo pip3 install --upgrade pip
-install python-pip
+
+log_info "安装pip2"
+install epel-release python-pip
 sudo pip install --upgrade pip
 
 #python语法检测
